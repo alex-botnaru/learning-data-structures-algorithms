b29d21f7349ec91a8e715c3e54f61aecd82cc2ad
alexandru.botnaru <agbotnaru@gmail.com>
2022-09-12 19:45:54 -0500

    Implemented the Product of array except self and top K frequent elements problem.

--- problems/README.md
+++ problems/README.md
@@ -8,3 +8,6 @@
 2. Valid Anagram: https://leetcode.com/problems/valid-anagram/
 3. Two Sum : https://leetcode.com/problems/two-sum/
 4. Group Anagrams: https://leetcode.com/problems/group-anagrams/
+5. Top K Frequent Elements: https://leetcode.com/problems/top-k-frequent-elements/
+6. Product of Array Except Self: https://leetcode.com/problems/product-of-array-except-self/
+7. 
---
+++ problems/src/main/java/com/abotnaru/problems/neetcode/ProductOfArrayExceptSelf.java
@@ -0,0 +1,37 @@
+package com.abotnaru.problems.neetcode;
+
+public class ProductOfArrayExceptSelf {
+
+	/**
+	 * Return an array such that any element in the array is equal to the product of
+	 * all the elements in the input array except that element.
+	 * <p>
+	 * Time complexity is O(N). Space complexity is O(1) if not considering the
+	 * output array as an extra space.
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	public int[] productExceptSelf(int[] nums) {
+
+		int[] result = new int[nums.length];
+
+		// Loop from start to end. At the current index store the previous product
+		// (default value 1)
+		int prefix = 1;
+		for (int i = 0; i < result.length; i++) {
+			result[i] = prefix;
+			prefix *= nums[i];
+		}
+
+		// Loop from end to beginning. Multiply the current element with the next
+		// product value (computed from the end, default value 1)
+		int postfix = 1;
+		for (int i = result.length - 1; i >= 0; i--) {
+			result[i] *= postfix;
+			postfix *= nums[i];
+		}
+
+		return result;
+	}
+}
